59a72a1fbb5632ac0ccc7f05ff6bb73b0db98c00
tlev159 <t.levente1598@gmail.com>
2021-08-02 19:13:25 +0200

    remove validation from DTO

--- src/main/java/com/tl/cemetery/grave/GraveDTO.java
+++ src/main/java/com/tl/cemetery/grave/GraveDTO.java
@@ -4,24 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GraveDTO {
 
-    @NotEmpty
-    @NotNull
-    @Column(nullable = false, length = 3)
+    private Long id;
+
     private String name;
 
-    @Column(nullable = false, length = 3)
     private int row;
 
-    @Column(nullable = false, length = 3)
     private int column;
 
 }
