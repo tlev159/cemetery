49f7a6402539ab7bd05daa5f6b19f0db93940ff7
tlev159 <t.levente1598@gmail.com>
2021-08-02 20:39:32 +0200

    add proof test not to allowed duplicated grave

--- src/test/java/com/tl/cemetery/GraveRestTemplateIT.java
+++ src/test/java/com/tl/cemetery/GraveRestTemplateIT.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@Sql
 public class GraveRestTemplateIT {
 
     private final String URL_FOR_QUERY = "/api/graves";
@@ -29,16 +32,35 @@ public class GraveRestTemplateIT {
     void createGraveThenFindById() {
 
         GraveDTO graveDTO =
-                template.postForObject(URL_FOR_QUERY, new CreateGraveCommand("B", 2,4), GraveDTO.class);
+                template.postForObject(URL_FOR_QUERY, new CreateGraveCommand("B", 2, 4), GraveDTO.class);
 
         Long id = graveDTO.getId();
 
         GraveDTO loadedGrave =
-                template.getForObject(URL_FOR_QUERY  + "/" + id, GraveDTO.class);
+                template.getForObject(URL_FOR_QUERY + "/" + id, GraveDTO.class);
 
         assertThat(loadedGrave)
                 .extracting(GraveDTO::getName)
                 .isEqualTo("B");
+    }
+
+    @Test
+    void createTwiceTheSameGraveAndThenThatOnlyOnceInTheDatabase() {
+
+        template.postForObject(URL_FOR_QUERY, new CreateGraveCommand("B", 2, 4), GraveDTO.class);
+        template.postForObject(URL_FOR_QUERY, new CreateGraveCommand("B", 2, 4), GraveDTO.class);
+
+        List<GraveDTO> loadedGraveDTOs =
+                template.exchange(URL_FOR_QUERY,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<GraveDTO>>() {
+                        }).getBody();
+
+        assertThat(loadedGraveDTOs)
+                .hasSize(1)
+                .extracting(GraveDTO::getName)
+                .containsExactly("B");
     }
 
 }
