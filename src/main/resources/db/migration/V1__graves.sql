78a0047cb64b9b691299ef432860a218212e9d6d
tlev159 <t.levente1598@gmail.com>
2021-08-02 19:09:37 +0200

    delete unnecessary enter

--- src/main/resources/db/migration/V1__graves.sql
+++ src/main/resources/db/migration/V1__graves.sql
@@ -1,6 +1,5 @@
-CREATE TABLE graves
-(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE `graves` (
+    id  BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (3) NOT NULL,
     grave_row INT (3) NOT NULL,
     grave_column INT (3) NOT NULL
