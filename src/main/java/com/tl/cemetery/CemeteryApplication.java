d72f26567a381c93a34faae4e1d502614b0b3966
tlev159 <t.levente1598@gmail.com>
2021-08-02 14:49:51 +0200

    add mappers to Application

--- src/main/java/com/tl/cemetery/CemeteryApplication.java
+++ src/main/java/com/tl/cemetery/CemeteryApplication.java
@@ -1,13 +1,26 @@
 package com.tl.cemetery;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CemeteryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CemeteryApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().findAndRegisterModules();
     }
 
 }
