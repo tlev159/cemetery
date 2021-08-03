81a75e525a6083fa8bda44e3bf5b6af59dabac19
tlev159 <t.levente1598@gmail.com>
2021-08-03 21:40:38 +0200

    create post for obituary controller and service

--- src/main/java/com/tl/cemetery/onituary/ObituaryController.java
+++ src/main/java/com/tl/cemetery/onituary/ObituaryController.java
@@ -1,6 +1,8 @@
 package com.tl.cemetery.onituary;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,9 @@ public class ObituaryController {
 
     private ObituaryService service;
 
+    @PostMapping
+    public ObituaryDTO createObituary(@RequestBody CreateObituaryCommand command) {
+        return service.createObituary(command);
+    }
 
 }
--- src/main/java/com/tl/cemetery/onituary/ObituaryService.java
+++ src/main/java/com/tl/cemetery/onituary/ObituaryService.java
@@ -1,8 +1,11 @@
 package com.tl.cemetery.onituary;
 
+import com.tl.cemetery.grave.Grave;
+import com.tl.cemetery.grave.GraveRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
@@ -11,6 +14,22 @@ public class ObituaryService {
     private ModelMapper modelMapper;
 
     private ObituaryRepository repository;
+    private GraveRepository graveRepository;
+
+    @Transactional
+    public ObituaryDTO createObituary(CreateObituaryCommand command) {
+        Obituary obituaryTemplate = new Obituary(command);
+        Grave graveTemplate = graveRepository.findById(command.getGrave().getId()).orElseThrow(() -> new IllegalArgumentException("Grave not found"));
+        if (repository.findAll().stream().noneMatch(o -> o.getName().equalsIgnoreCase(command.getName())
+            && o.getNameOfMother().equalsIgnoreCase(command.getNameOfMother())
+            && o.getDateOfBirth().equals(command.getDateOfBirth()))) {
+            repository.save(obituaryTemplate);
+
+        }
+        graveTemplate.addObituary(obituaryTemplate);
+        return modelMapper.map(obituaryTemplate, ObituaryDTO.class);
+    }
+
 
 
 }
