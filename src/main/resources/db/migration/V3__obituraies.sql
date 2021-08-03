6aa2fbbda8d72e9d7cae99c00b05ffea67376e45
tlev159 <t.levente1598@gmail.com>
2021-08-03 22:25:49 +0200

    change column name

--- src/main/resources/db/migration/V3__obituraies.sql
+++ src/main/resources/db/migration/V3__obituraies.sql
@@ -2,7 +2,7 @@ CREATE TABLE obituaries (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(200) NOT NULL,
   name_of_mother VARCHAR(200) NOT NULL,
-  day_of_birth DATE NOT NULL,
+  date_of_birth DATE NOT NULL,
   date_of_rip DATE NOT NULL,
   grave_id BIGINT,
   CONSTRAINT fk_grave_o_id FOREIGN KEY (grave_id) REFERENCES graves (id)
