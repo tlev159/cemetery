c96831aa83f9d53daadb0bc7fedcbfd22ba2fe0e
tlev159 <t.levente1598@gmail.com>
2021-08-03 20:56:32 +0200

    add obituary entity

---
+++ src/main/java/com/tl/cemetery/onituary/Obituary.java
@@ -0,0 +1,40 @@
+package com.tl.cemetery.onituary;
+
+import com.tl.cemetery.grave.Grave;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "obituaries")
+public class Obituary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String nameOfMother;
+
+    private LocalDate dateOfBirth;
+
+    private LocalDate dateOfRIP;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Grave grave;
+
+    public Obituary(String name, String nameOfMother, LocalDate dateOfBirth, LocalDate dateOfRIP) {
+        this.name = name;
+        this.nameOfMother = nameOfMother;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfRIP = dateOfRIP;
+    }
+
+}
