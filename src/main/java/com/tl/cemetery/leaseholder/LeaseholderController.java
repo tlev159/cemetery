2167015b79ad20682d8de61683db97e053df215f
tlev159 <t.levente1598@gmail.com>
2021-08-03 16:53:55 +0200

    add swagger tag to class

--- src/main/java/com/tl/cemetery/leaseholder/LeaseholderController.java
+++ src/main/java/com/tl/cemetery/leaseholder/LeaseholderController.java
@@ -1,5 +1,6 @@
 package com.tl.cemetery.leaseholder;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/leaseholder")
+@Tag(name = "Operations with leaseholders", description = "Operations with leaseholders")
 public class LeaseholderController {
 
-    private LeaserholderService service;
+    private LeaseholderService service;
+
 
 
 }
