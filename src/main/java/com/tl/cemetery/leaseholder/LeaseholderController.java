2ab4a03c579f36160d2e4220886ba56ac8d39737
tlev159 <t.levente1598@gmail.com>
2021-08-03 17:11:34 +0200

    add create leaseholder to controller

--- src/main/java/com/tl/cemetery/leaseholder/LeaseholderController.java
+++ src/main/java/com/tl/cemetery/leaseholder/LeaseholderController.java
@@ -2,8 +2,12 @@ package com.tl.cemetery.leaseholder;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @AllArgsConstructor
 @RestController
@@ -13,6 +17,9 @@ public class LeaseholderController {
 
     private LeaseholderService service;
 
-
+    @PostMapping
+    private LeaseholderDTO createLeaseholder(@Valid @RequestBody CreateLeaseholderCommand command) {
+        return service.createLeaseholder(command);
+    }
 
 }
