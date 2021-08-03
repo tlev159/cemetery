0a18a110edf7fa335dc094591189be830f36ab1a
tlev159 <t.levente1598@gmail.com>
2021-08-03 17:44:47 +0200

    add swagger ui descriptions

--- src/main/java/com/tl/cemetery/leaseholder/CreateLeaseholderCommand.java
+++ src/main/java/com/tl/cemetery/leaseholder/CreateLeaseholderCommand.java
@@ -1,6 +1,8 @@
 package com.tl.cemetery.leaseholder;
 
 import com.tl.cemetery.grave.GraveType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,26 +15,33 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Tag(name = "create leaseholder", description = "create leaseholder")
 public class CreateLeaseholderCommand {
 
     @NotBlank
     @Length(min = 5, max = 200)
+    @Schema(description = "add leaseholders name", example = "John Sample")
     private String name;
 
     @NotBlank
     @Length(min = 5, max = 255)
+    @Schema(description = "add address to leaseholder", example = "1212 OverTheRainbow, Backer street 13.")
     private String address;
 
     @NotBlank
     @Length(min = 5, max = 100)
+    @Schema(description = "add phone number to leaseholder", example = "+36-1/123-4567")
     private String telephone;
 
     @NotNull
+    @Schema(description = "add date for funeral", example = "2020-08-20")
     private LocalDate leasedAt;
 
     @NotNull
+    @Schema(description = "add type of the grave", example = "STONE")
     private GraveType type;
 
     @NotNull
+    @Schema(description = "add id of grave", example = "3")
     private Long graveId;
 }
