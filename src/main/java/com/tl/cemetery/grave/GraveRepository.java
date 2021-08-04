d33c71cd765e1d115d97ca7956662d22be247a1c
tlev159 <t.levente1598@gmail.com>
2021-08-04 15:21:34 +0200

    add find grave by command to repository

--- src/main/java/com/tl/cemetery/grave/GraveRepository.java
+++ src/main/java/com/tl/cemetery/grave/GraveRepository.java
@@ -12,4 +12,6 @@ public interface GraveRepository extends JpaRepository<Grave, Long> {
     @Query(value = "select g from Grave g where g.name like :name% order by g.name, g.row, g.column")
     List<Grave> findAllGravesInParcel(@Param("name") String name);
 
+    @Query(value = "select g from Grave g where g.name = :name and g.row = :row and g.column = :column")
+    Grave findGraveByCommand(String name, int row, int column);
 }
