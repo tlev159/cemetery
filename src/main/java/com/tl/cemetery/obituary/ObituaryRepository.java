51c955bbe455db5b8874163b290754fe54d1313a
tlev159 <t.levente1598@gmail.com>
2021-08-04 17:34:17 +0200

    add jpql query for list all in given grave

--- src/main/java/com/tl/cemetery/obituary/ObituaryRepository.java
+++ src/main/java/com/tl/cemetery/obituary/ObituaryRepository.java
@@ -1,7 +1,15 @@
 package com.tl.cemetery.obituary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface ObituaryRepository extends JpaRepository<Obituary, Long> {
 
+
+    @Query(value = "select o from Obituary o where o.grave.id = :id order by o.name")
+    List<ObituaryDTO> findAllInGraveWithGraveId(@Param("id") Long id);
 }
