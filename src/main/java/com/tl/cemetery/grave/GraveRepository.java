e475b3b37c323f01487b74f2d74e95a69c045245
tlev159 <t.levente1598@gmail.com>
2021-08-04 22:16:40 +0200

    change jpql query with nested join fetch

--- src/main/java/com/tl/cemetery/grave/GraveRepository.java
+++ src/main/java/com/tl/cemetery/grave/GraveRepository.java
@@ -1,5 +1,6 @@
 package com.tl.cemetery.grave;
 
+import com.tl.cemetery.obituary.Obituary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +13,6 @@ public interface GraveRepository extends JpaRepository<Grave, Long> {
     @Query(value = "select g from Grave g where g.name like :name% order by g.name, g.row, g.column")
     List<Grave> findAllGravesInParcel(@Param("name") String name);
 
-    @Query(value = "select g from Grave g where g.name = :name and g.row = :row and g.column = :column")
-    Grave findGraveByCommand(String name, int row, int column);
+    @Query(value = "select o from Obituary o join fetch Grave g on o.grave.id = g.id where o.grave.id = (select g.id from Grave g where g.name = :name and g.row = :row and g.column = :column)")
+    List<Obituary> findGraveByCommand(@Param("name") String name, @Param("row") int row, @Param("column") int column);
 }
