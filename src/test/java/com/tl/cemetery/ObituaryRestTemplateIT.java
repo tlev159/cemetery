c8550f7b8d2b6a4bff393d1bad004e4cd63f3627
tlev159 <t.levente1598@gmail.com>
2021-08-04 18:27:43 +0200

    add create more then list all test

--- src/test/java/com/tl/cemetery/ObituaryRestTemplateIT.java
+++ src/test/java/com/tl/cemetery/ObituaryRestTemplateIT.java
@@ -13,8 +13,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -92,5 +95,50 @@ public class ObituaryRestTemplateIT {
 
     }
 
+    @Test
+    void createMoreThenListAll() {
 
+        GraveDTO graveDTO1 =
+                template.postForObject(URL_FOR_GRAVES, new CreateGraveCommand("B", 1, 4), GraveDTO.class);
+        GraveDTO graveDTO2 =
+                template.postForObject(URL_FOR_GRAVES, new CreateGraveCommand("A", 1, 4), GraveDTO.class);
+
+        Long id1 = graveDTO1.getId();
+        Long id2 = graveDTO2.getId();
+
+        template.postForObject(URL_FOR_OBITUARIES,
+                new CreateObituaryCommand("Minta Aladár",
+                        "Csendes Ilonka", LocalDate.of(1945, 3, 8),
+                        LocalDate.of(2020, 3, 8), id1),
+                ObituaryDTO.class);
+        template.postForObject(URL_FOR_OBITUARIES,
+                new CreateObituaryCommand("Minta Béla",
+                        "Jane Doe", LocalDate.of(1945, 3, 8),
+                        LocalDate.of(2020, 3, 8), id2),
+                ObituaryDTO.class);
+        template.postForObject(URL_FOR_OBITUARIES,
+                new CreateObituaryCommand("Minta Cecília",
+                        "Hangos Ilonka", LocalDate.of(1945, 3, 8),
+                        LocalDate.of(2020, 3, 8), id1),
+                ObituaryDTO.class);
+        template.postForObject(URL_FOR_OBITUARIES,
+                new CreateObituaryCommand("Minta Dezső",
+                        "Rendes Ilonka", LocalDate.of(1945, 3, 8),
+                        LocalDate.of(2020, 3, 8), id2),
+                ObituaryDTO.class);
+
+
+        List<ObituaryDTO> loadedObituaryDTOs =
+                template.exchange(URL_FOR_OBITUARIES,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<ObituaryDTO>>() {
+                        }).getBody();
+
+        assertThat(loadedObituaryDTOs)
+                .hasSize(4)
+                .extracting(ObituaryDTO::getName)
+                .containsExactly("Minta Aladár","Minta Béla", "Minta Cecília", "Minta Dezső");
+
+    }
 }
