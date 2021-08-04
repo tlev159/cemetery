80edd7de4a5fefb091225d3aa471b137b8ba9101
tlev159 <t.levente1598@gmail.com>
2021-08-04 17:21:39 +0200

    add validator to name

--- src/main/java/com/tl/cemetery/obituary/Obituary.java
+++ src/main/java/com/tl/cemetery/obituary/Obituary.java
@@ -19,6 +19,7 @@ public class Obituary {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
